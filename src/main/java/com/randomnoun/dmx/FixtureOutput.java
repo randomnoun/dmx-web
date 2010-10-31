4ab6c594b2499bda462dea62d9c9161fac4823f8
Greg Knox <knoxg@randomnoun.com>
2010-10-31 20:54:20 +0000

    added pan and tilt to fixture output; added fixture mask constants

--- src/main/java/com/randomnoun/dmx/FixtureOutput.java
+++ src/main/java/com/randomnoun/dmx/FixtureOutput.java
@@ -8,8 +8,28 @@ import java.awt.Color;
  */
 public abstract class FixtureOutput {
 
+	/** A color mask */ 
+	public int MASK_COLOR = 1;
+	
+	/** A time mask */
+	public int MASK_TIME = 2;
+	
+	/** A pan mask */
+	public int MASK_PAN = 4;
+	
+	/** A tilt mask */
+	public int MASK_TILT = 8;
+	
+	/** A mask containing no fixture attributes */
+	public int MASK_NONE = 0;
+	
+	/** A mask containing all fixture attributes (color, time, pan and tilt) */
+	public int MASK_ALL=MASK_COLOR | MASK_TIME | MASK_PAN | MASK_TILT;
+	
 	public abstract Color getColor();
 	public abstract long getTime();
+	public abstract Double getPan();   /* pan value, in degrees */
+	public abstract Double getTilt();  /* pan value, in degrees */
 	
 	// TODO: what direction is it pointing in, what gobos has it got on, etc...
 	// could get interesting for fixtures with multiple heads, but we'll get to that later
