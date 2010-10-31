fbc144a161567b9e1edf59582f81396b1b06f9f2
Greg Knox <knoxg@randomnoun.com>
2010-10-31 20:57:19 +0000

    common code between 4 and 12 channel mini-wash fixture definition

---
+++ src/main/java/com/randomnoun/dmx/scripted/MiniWashFixtureDefBase.java
@@ -0,0 +1,30 @@
+package com.randomnoun.dmx.scripted;
+
+import com.randomnoun.dmx.Fixture;
+import com.randomnoun.dmx.FixtureController;
+import com.randomnoun.dmx.FixtureDef;
+import com.randomnoun.dmx.channelMuxer.ChannelMuxer;
+import com.randomnoun.dmx.lightSource.HexagonLedGridDef;
+
+public abstract class MiniWashFixtureDefBase extends FixtureDef {
+
+	public MiniWashFixtureDefBase() {
+		this.vendor = "Chinese sweatshop workers";
+		this.model = "Miniwash";
+		this.length = 300;
+		this.width = 240;
+		this.height= 240;
+		this.weight = 3800;
+		this.panRange = 540;
+		this.tiltRange = 180;
+		this.maxAmbientTemperature = 40;
+		this.maxWattage = 68;
+		this.lightSourceDef = new HexagonLedGridDef(3,
+		  "  R G B " +
+		  " R G B R " +
+		  "B G x R B" +
+		  " G R B G" +
+		  "  R B G ");
+	}
+
+}
