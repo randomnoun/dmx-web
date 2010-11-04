0a7d0257603d96b2ade147a61dfe5c6d796c4d6b
Greg Knox <knoxg@randomnoun.com>
2010-11-04 02:16:29 +0000

    USB Pro COM test

--- src/main/java/com/randomnoun/dmx/com/TestVBCOM.java
+++ src/main/java/com/randomnoun/dmx/com/TestVBCOM.java
@@ -3,15 +3,14 @@ package com.randomnoun.dmx.com;
 import com.jacob.activeX.ActiveXComponent;
 import com.jacob.com.SafeArray;
 import com.jacob.com.Variant;
-import com.jacobgen.opendmx._OpenDMXCom;
+import com.jacobgen.dmx._OpenDMXCom;
+import com.jacobgen.dmx._USBDMXProCom;
 
 // should be using enttec usb pro interface, not opendmx
 // so that's interesting. 
 
 public class TestVBCOM {
 
-	private ActiveXComponent axc;
-	private _OpenDMXCom openDmxCom;
 
 	/*
 	public static byte toByte(int i) {
@@ -19,7 +18,10 @@ public class TestVBCOM {
 	}
 	*/
 	
-	public void testCom() {
+	public void testOpenDmxCom() {
+		ActiveXComponent axc;
+		_OpenDMXCom openDmxCom;
+
 		axc = new ActiveXComponent("OpenDMX.OpenDMXCom");
 		openDmxCom = new _OpenDMXCom(axc);
 		
@@ -48,21 +50,21 @@ public class TestVBCOM {
 				for (int i=0; i<512; i++) { universe[i] = 0; }
 				SafeArray safeArray = new SafeArray(Variant.VariantByte, 512);
 				safeArray.fromByteArray(universe);
-				openDmxCom.set_DMX(enabledDevice, safeArray);
+				openDmxCom.setDMXValues(enabledDevice, safeArray);
 				
 				try { Thread.sleep(1000); } catch (InterruptedException e) { }
 				System.out.println("Setting DMX channel 11 and 12 to 255");
 				universe[11] = (byte) 255;
 				universe[12] = (byte) 255;
 				safeArray.fromByteArray(universe);
-				openDmxCom.set_DMX(enabledDevice, safeArray);
+				openDmxCom.setDMXValues(enabledDevice, safeArray);
 				
 				try { Thread.sleep(1000); } catch (InterruptedException e) { }
 				System.out.println("Setting DMX channel 11 and 12 to 0");
 				universe[11] = (byte) 255;
 				universe[12] = (byte) 255;
 				safeArray.fromByteArray(universe);
-				openDmxCom.set_DMX(enabledDevice, safeArray);
+				openDmxCom.setDMXValues(enabledDevice, safeArray);
 				
 				
 			}
@@ -77,10 +79,76 @@ public class TestVBCOM {
 			}
 		}
 	}
+
+	public void testUSBDMXProCom() {
+		ActiveXComponent axc;
+		_USBDMXProCom usbDMXPro;
+
+		axc = new ActiveXComponent("OpenDMX.USBDMXPro");
+		usbDMXPro = new _USBDMXProCom(axc);
+		
+		try {
+			usbDMXPro.init(4);
+			// usbDMXPro.init_All(); // automatically find devices and start threads for them
+			
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			int enabledDevice = -1;
+			
+			for (int i=4; i<=4; i++) {
+				String errorString = usbDMXPro.getErrorString(i);
+				System.out.println("Error " + i + ": " + errorString);
+				// if (errorString.equals("SENDING DMX")) { enabledDevice = i; }
+				enabledDevice = 4;
+			}
+			
+			byte[] universe = new byte[512];
+			if (enabledDevice!=-1) { 
+				System.out.println("Setting DMX values to 0");
+				for (int i=0; i<512; i++) { universe[i] = 0; }
+				SafeArray safeArray = new SafeArray(Variant.VariantByte, 512);
+				safeArray.fromByteArray(universe);
+				usbDMXPro.setDMXValues(enabledDevice, safeArray);
+				usbDMXPro.send(enabledDevice);
+				
+				try { Thread.sleep(1000); } catch (InterruptedException e) { }
+				System.out.println("Setting DMX channel 11 and 12 to 255");
+				universe[11] = (byte) 255;
+				universe[12] = (byte) 255;
+				safeArray.fromByteArray(universe);
+				usbDMXPro.setDMXValues(enabledDevice, safeArray);
+				usbDMXPro.send(enabledDevice);
+				
+				try { Thread.sleep(1000); } catch (InterruptedException e) { }
+				System.out.println("Setting DMX channel 11 and 12 to 0");
+				universe[11] = (byte) 255;
+				universe[12] = (byte) 255;
+				safeArray.fromByteArray(universe);
+				usbDMXPro.setDMXValues(enabledDevice, safeArray);
+				usbDMXPro.send(enabledDevice);
+				
+			}
+			
+		} finally {
+			if (usbDMXPro != null) {
+				System.out.println("Closing COM object");
+
+				// openDmxCom.stop_Threads();
+				usbDMXPro.done(4);
+			}
+		}
+	}
+	
 	
 	public static void main(String args[]) {
 		TestVBCOM testClass = new TestVBCOM();
-		testClass.testCom();
+		// testClass.testOpenDmxCom();
+		testClass.testUSBDMXProCom();
 		
 	}
 	
