48774665b356c0d87208e542ba3939b4d51d5133
Greg Knox <knoxg@randomnoun.com>
2010-11-04 12:53:17 +0000

    javadoc

--- src/main/java/com/randomnoun/dmx/channelMuxer/MaskChannelMuxer.java
+++ src/main/java/com/randomnoun/dmx/channelMuxer/MaskChannelMuxer.java
@@ -13,7 +13,11 @@ import com.randomnoun.dmx.channelMuxer.timed.StrobeChannelMuxer;
 import com.randomnoun.dmx.channel.StrobeChannelDef;
 
 /** A muxer which takes a set of muxers as input, and
- * combines them into a single FixtureOutput
+ * combines them into a single FixtureOutput.
+ * 
+ * e.g. it might take the color from one muxer, the tilt 
+ * position from another muxer, and the pan position from another
+ * muxer. 
  * 
  * @author knoxg
  */
@@ -22,6 +26,20 @@ public class MaskChannelMuxer extends ChannelMuxer {
 	ChannelMuxer[] inputMuxers;
 	Logger logger = Logger.getLogger(MaskChannelMuxer.class);
 	
+	/** Create a new MaskChannelMuxer. For each inputMuxer supplied
+	 * to the constructor, the output of that muxer is logically ANDed
+	 * with the inputMask to generate the output. 
+	 * 
+	 * <p>e.g. if inputMask[0] = FixtureOutput.MASK_PAN | FixtureOutput.MASK_TILT,
+	 * then the pan and tilt of the 0'th muxer will be passed through 
+	 * to the output of the muxer.
+	 * 
+	 * <p>If >1 muxer contribute to an output attribute, then the
+	 * last one wins. 
+	 * 
+	 * @param inputMasks
+	 * @param inputMuxers
+	 */
 	public MaskChannelMuxer(int[] inputMasks, ChannelMuxer[] inputMuxers) {
 		super(inputMuxers[0].getFixture());
 		// @TODO check all inputMuxers refer to the same fixture
