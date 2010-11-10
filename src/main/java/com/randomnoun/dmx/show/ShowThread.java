04913925b5205722f2ff9596fdb68daa79121b54
Greg Knox <knoxg@randomnoun.com>
2010-11-10 02:51:56 +0000

    allow showThread to kick off another show if current one is cancelled, completes, or an exception occurs

--- src/main/java/com/randomnoun/dmx/show/ShowThread.java
+++ src/main/java/com/randomnoun/dmx/show/ShowThread.java
@@ -3,6 +3,7 @@ package com.randomnoun.dmx.show;
 import org.apache.log4j.Logger;
 
 import com.randomnoun.dmx.config.AppConfig;
+import com.randomnoun.dmx.config.AppConfig.AppConfigState;
 
 public class ShowThread extends Thread {
 
@@ -14,19 +15,39 @@ public class ShowThread extends Thread {
 		this.show = show;
 	}
 	
-	// @TODO do something with exceptions that occur in shows here
+
 	public void run() {
 		logger.debug("Playing show '" + show.getName() + "'");
+		AppConfig appConfig = AppConfig.getAppConfig();
+		long onCancelShowId = show.getOnCancelShowId();
+		long onCompleteShowId = show.getOnCompleteShowId();
 		try {
+			show.internalReset();
 			show.play();
 			if (show.isCancelled()) { 
 				logger.debug("Show '" + show.getName() + "' was cancelled");
+				// @TODO if this is the same show (*really bad idea*), then just loop this method
+				if (onCancelShowId!=-1 && appConfig.getAppConfigState()==AppConfigState.RUNNING) {
+					logger.debug("Show '" + show.getName() + "' requesting onCancelShowId " + onCancelShowId + " to start");
+					AppConfig.getAppConfig().startShow((int) onCancelShowId);
+				}
 			} else {
 				logger.debug("Show '" + show.getName() + "' completed");
+				if (onCompleteShowId!=-1 && appConfig.getAppConfigState()==AppConfigState.RUNNING) {
+					logger.debug("Show '" + show.getName() + "' requesting onCompletedShowId " + onCompleteShowId + " to start");
+					AppConfig.getAppConfig().startShow((int) onCompleteShowId);
+				}
+
 			}
 		} catch (Exception e) {
 			logger.debug("Show '" + show.getName() + "' threw an exception", e);
+			show.setLastException(e);
 			AppConfig.getAppConfig().addShowException(show, e);
+			if (onCancelShowId!=-1 && appConfig.getAppConfigState()==AppConfigState.RUNNING) {
+				logger.debug("Show '" + show.getName() + "' requesting onCancelShowId " + onCancelShowId + " to start due to exception");
+				AppConfig.getAppConfig().startShow((int) onCancelShowId);
+			}
+
 		}
 	}
 	
