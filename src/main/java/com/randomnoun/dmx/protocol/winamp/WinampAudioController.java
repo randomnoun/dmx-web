62605c858d2a9de3e0260de45c07104d98ebc3cd
Greg Knox <knoxg@randomnoun.com>
2010-11-16 02:21:41 +0000

    only send commands to winamp if connection was initially successful (TODO: retries)

--- src/main/java/com/randomnoun/dmx/protocol/winamp/WinampAudioController.java
+++ src/main/java/com/randomnoun/dmx/protocol/winamp/WinampAudioController.java
@@ -29,6 +29,7 @@ public class WinampAudioController extends AudioController
 	int timeout;
 	String password;
 	String defaultPath;
+	boolean connected = false;
 	
 	NGWinAmp winamp;
 	
@@ -42,11 +43,13 @@ public class WinampAudioController extends AudioController
 		if (defaultPath != null && !defaultPath.endsWith("/")) {
 			defaultPath += "/";
 		}
+		connected = false;
 		exceptionContainer = new ExceptionContainerImpl();
 		winamp = new NGWinAmp(host, port, timeout, true);
 		try {
 			winamp.connect();
 			winamp.authenticate(password);
+			connected = true;
 		} catch (Exception e) {
 			logger.error("Exception connecting to Winamp", e);
 			exceptionContainer.addException(new RuntimeException("Exception connecting to Winamp"));			
@@ -63,44 +66,24 @@ public class WinampAudioController extends AudioController
 	@Override
 	public void playAudioFile(String filename) {
 		logger.debug("Playing audio file '" + filename + "'");
-		try {
-			File file = new File(filename);
-			if (!file.isAbsolute() && defaultPath!=null) { 
-				file = new File(new File(defaultPath), filename);
-				filename = file.getCanonicalPath();
-			}
-			filename = Text.replaceString(filename, "\\", "/");
-			
-			
-			String[] files = winamp.sendGetFiles(NGWinAmp.NGWINAMP_ALL);
-			int playlistPosition = -1;
-			for (int i=0; i<files.length; i++) {
-				if (files[i].equals(filename)) {
-					playlistPosition = i; break;
+		if (connected) {
+			try {
+				File file = new File(filename);
+				if (!file.isAbsolute() && defaultPath!=null) { 
+					file = new File(new File(defaultPath), filename);
+					filename = file.getCanonicalPath();
 				}
-			}
-			if (playlistPosition==-1) {
-				String newFiles[] = new String[] { filename };
-				winamp.sendAddFiles(newFiles);
-				files = winamp.sendGetFiles(NGWinAmp.NGWINAMP_ALL);
+				filename = Text.replaceString(filename, "\\", "/");
+				
+				
+				String[] files = winamp.sendGetFiles(NGWinAmp.NGWINAMP_ALL);
+				int playlistPosition = -1;
 				for (int i=0; i<files.length; i++) {
 					if (files[i].equals(filename)) {
 						playlistPosition = i; break;
 					}
 				}
-			} else {
-				// the check-end-of-list here should work but doesn't
-				//if (playlistPosition!=files.length-1) {
-					
-					// we can't play audio at this position, since it
-					// will then go on to play everything else in the playlist.
-					
-					// attempt 1: swap with position at end of list (doesn't work)
-					// winamp.sendSwap(playlistPosition, files.length-1); 
-					
-					// attempt 2: delete and re-add audio file (doesn't work and throws a SocketException)
-					/*
-					winamp.sendDeletePlaylist(new int[] { playlistPosition });
+				if (playlistPosition==-1) {
 					String newFiles[] = new String[] { filename };
 					winamp.sendAddFiles(newFiles);
 					files = winamp.sendGetFiles(NGWinAmp.NGWINAMP_ALL);
@@ -109,58 +92,84 @@ public class WinampAudioController extends AudioController
 							playlistPosition = i; break;
 						}
 					}
-					*/
-					
-					// attempt 3: clear playlist and re-add file
-					// (not great, since whoever's on the desk will need to reset the playlist
-					// when people start getting their groove on, but does seem to work)
-					winamp.sendClearPlaylist();
-					String newFiles[] = new String[] { filename };
-					winamp.sendAddFiles(newFiles);
-					files = winamp.sendGetFiles(NGWinAmp.NGWINAMP_ALL);
-					for (int i=0; i<files.length; i++) {
-						if (files[i].equals(filename)) {
-							playlistPosition = i; break;
+				} else {
+					// the check-end-of-list here should work but doesn't
+					//if (playlistPosition!=files.length-1) {
+						
+						// we can't play audio at this position, since it
+						// will then go on to play everything else in the playlist.
+						
+						// attempt 1: swap with position at end of list (doesn't work)
+						// winamp.sendSwap(playlistPosition, files.length-1); 
+						
+						// attempt 2: delete and re-add audio file (doesn't work and throws a SocketException)
+						/*
+						winamp.sendDeletePlaylist(new int[] { playlistPosition });
+						String newFiles[] = new String[] { filename };
+						winamp.sendAddFiles(newFiles);
+						files = winamp.sendGetFiles(NGWinAmp.NGWINAMP_ALL);
+						for (int i=0; i<files.length; i++) {
+							if (files[i].equals(filename)) {
+								playlistPosition = i; break;
+							}
 						}
-					}
-					
-				//}
+						*/
+						
+						// attempt 3: clear playlist and re-add file
+						// (not great, since whoever's on the desk will need to reset the playlist
+						// when people start getting their groove on, but does seem to work)
+						winamp.sendClearPlaylist();
+						String newFiles[] = new String[] { filename };
+						winamp.sendAddFiles(newFiles);
+						files = winamp.sendGetFiles(NGWinAmp.NGWINAMP_ALL);
+						for (int i=0; i<files.length; i++) {
+							if (files[i].equals(filename)) {
+								playlistPosition = i; break;
+							}
+						}
+						
+					//}
+				}
+				if (playlistPosition==-1) {
+					logger.error("File not added to WinAMP playlist");
+					exceptionContainer.addException(new RuntimeException("File not added to WinAMP playlist"));
+				} else {
+					winamp.sendSetRepeat(false);
+					winamp.sendSetPlaylistPosition(playlistPosition);
+					winamp.sendSetPosition(0);
+					winamp.sendPlay();
+					winamp.sendSetVolume(100.0);
+				}
+			} catch (IOException e) {
+				logger.error(e);
+				exceptionContainer.addException(new RuntimeException("Exception playing audio file", e));
 			}
-			if (playlistPosition==-1) {
-				logger.error("File not added to WinAMP playlist");
-				exceptionContainer.addException(new RuntimeException("File not added to WinAMP playlist"));
-			} else {
-				winamp.sendSetRepeat(false);
-				winamp.sendSetPlaylistPosition(playlistPosition);
-				winamp.sendSetPosition(0);
-				winamp.sendPlay();
-				winamp.sendSetVolume(100.0);
-			}
-		} catch (IOException e) {
-			logger.error(e);
-			exceptionContainer.addException(new RuntimeException("Exception playing audio file", e));
 		}
 	}
 
 	@Override
 	public void stopAudio() {
 		logger.debug("Stopping audio");
-		try {
-			winamp.sendStop();
-		} catch (IOException e) {
-			logger.error(e);
-			exceptionContainer.addException(new RuntimeException("Exception stopping audio file", e));
+		if (connected) {
+			try {
+				winamp.sendStop();
+			} catch (IOException e) {
+				logger.error(e);
+				exceptionContainer.addException(new RuntimeException("Exception stopping audio file", e));
+			}
 		}
 	}
 
 	@Override
 	public void setVolume(double volumePercent) {
 		logger.debug("Setting volume");
-		try {
-			winamp.sendSetVolume(volumePercent);
-		} catch (IOException e) {
-			logger.error(e);
-			exceptionContainer.addException(new RuntimeException("Exception stopping audio file", e));
+		if (connected) {
+			try {
+				winamp.sendSetVolume(volumePercent);
+			} catch (IOException e) {
+				logger.error(e);
+				exceptionContainer.addException(new RuntimeException("Exception stopping audio file", e));
+			}
 		}
 	}
 
@@ -175,10 +184,12 @@ public class WinampAudioController extends AudioController
 
 	@Override
 	public void close() {
-		try {
-			winamp.disconnect();
-		} catch (IOException ioe) {
-			logger.error("Exception closing NGWinAmp audioController", ioe);
+		if (connected) {
+			try {
+				winamp.disconnect();
+			} catch (IOException ioe) {
+				logger.error("Exception closing NGWinAmp audioController", ioe);
+			}
 		}
 	}
 
