5075b4a0123978c89ed0c7af9f577459777337ce
Greg Knox <knoxg@randomnoun.com>
2010-11-23 11:21:12 +0000

    show that fades the dimmers based on audio output

---
+++ src/main/java/com/example/dmx/show/AudioFaderShow.java
@@ -0,0 +1,54 @@
+package com.example.dmx.show;
+
+import java.awt.Color;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+import com.randomnoun.dmx.AudioSource;
+import com.randomnoun.dmx.Controller;
+import com.randomnoun.dmx.fixture.Fixture;
+import com.randomnoun.dmx.fixture.FixtureController;
+import com.randomnoun.dmx.show.Show;
+
+/** dim the lights based on the bass output
+ * 
+ * @author knoxg
+ */
+public class AudioFaderShow extends Show {
+
+	Logger logger = Logger.getLogger(AudioFaderShow.class);
+
+	Fixture fixture1;
+	Fixture fixture2;
+	FixtureController fix1;
+	FixtureController fix2;
+	
+	public AudioFaderShow(long id, Controller controller, Map properties) {
+		super(id, controller, "Audio Fader", Long.MAX_VALUE, properties);
+		
+		fixture1 = controller.getFixtureByName("leftWash"); fix1 = fixture1.getFixtureController();
+		fixture2 = controller.getFixtureByName("rightWash"); fix2 = fixture2.getFixtureController();
+	}
+	
+	public void pause() {}
+	public void stop() {}
+	
+	protected void reset() {
+		super.reset();
+		logger.debug("reset()");
+	}
+	
+	public void play() {
+		logger.debug("play()");
+		AudioSource audioSource = getController().getAudioSource();
+		while (!isCancelled()) {
+			int fade = (int) Math.min(audioSource.getBassMidTreble()[0]*255, 255);
+			Color color = new Color(fade, fade, fade);
+			fix1.setColor(color);
+			fix2.setColor(color);
+			waitFor(30); 
+		}
+		logger.debug("play() completed");
+	}
+}	
