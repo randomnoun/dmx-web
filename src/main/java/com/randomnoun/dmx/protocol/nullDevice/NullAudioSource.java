8b5773af68e7e00d13d0ea0be23c3770289d1724
Greg Knox <knoxg@randomnoun.com>
2010-11-23 09:29:22 +0000

    null audioSource

---
+++ src/main/java/com/randomnoun/dmx/protocol/nullDevice/NullAudioSource.java
@@ -0,0 +1,59 @@
+package com.randomnoun.dmx.protocol.nullDevice;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+import com.randomnoun.dmx.AudioSource;
+import com.randomnoun.dmx.ExceptionContainer;
+import com.randomnoun.dmx.ExceptionContainerImpl;
+import com.randomnoun.dmx.protocol.ngWinamp.NGWinAmp;
+
+public class NullAudioSource extends AudioSource {
+
+	public static Logger logger = Logger.getLogger(NullAudioSource.class);
+	
+    private ExceptionContainerImpl exceptionContainer;
+    float bassMidTreble[];
+    float spectrum[][];
+    
+	public NullAudioSource(Map properties) {
+		super(properties);
+		exceptionContainer = new ExceptionContainerImpl();
+		bassMidTreble = new float[3];
+		spectrum = new float[2][256];
+	}
+
+	public List<TimestampedException> getExceptions() {
+		return exceptionContainer.getExceptions();
+	}
+
+	public void clearExceptions() {
+		exceptionContainer.clearExceptions();
+	}
+
+	@Override
+	public void open() {
+	}
+
+	@Override
+	public void close() {
+	}
+
+	@Override
+	public boolean getBeat() {
+		return false;
+	}
+
+	@Override
+	public float[] getBassMidTreble() {
+		return bassMidTreble;
+	}
+
+	@Override
+	public float[][] getSpectrum() {
+		return spectrum;
+	}
+
+}
