bffade6bfe1e82fd45e5a7fa4346b32014d79928
Greg Knox <knoxg@randomnoun.com>
2010-12-02 03:50:47 +0000

    CustomControls

---
+++ src/main/java/com/randomnoun/dmx/fixture/CustomControl.java
@@ -0,0 +1,46 @@
+package com.randomnoun.dmx.fixture;
+
+/** A custom control allows fixture-specific features (or programmer-created
+ * features) to be selected from the fixture panel. 
+ * 
+ * @author knoxg
+ */
+public class CustomControl {
+
+	public enum UIType { TOGGLE }
+
+	public abstract static class CustomControlCallback {
+		public void setValue(int value) { };
+	}
+	
+	private String label;
+	private UIType uiType;
+	private int value;
+	private CustomControlCallback callback;
+	
+	public CustomControl(String label, UIType uiType, CustomControlCallback callback) {
+		this.label = label;
+		this.uiType = uiType;
+		this.callback = callback;
+	}
+	
+	public UIType getUIType() { return uiType; }
+	public int getValue() { return value; }
+	public String getLabel() { return label; }
+	
+	// sets the value of this control, non-user initiated
+	// (e.g. to clear a set of mutually-exclusive toggles)
+	// should only be called from within CustomControlCallback methods. 
+	public void setValue(int value) { 
+		this.value = value; 
+	}
+	
+	// sets the value of this control, as if the user initiated it
+	// will call the supplied callback, which should then perform the
+	// requested action
+	public void setValueWithCallback(int value) {
+		this.value = value;
+		callback.setValue(value);
+	}
+	
+}
