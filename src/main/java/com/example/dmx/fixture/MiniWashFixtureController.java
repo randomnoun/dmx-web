9f5357c213810ae1e646e869bec2cb7b8e783db3
Greg Knox <knoxg@randomnoun.com>
2010-12-02 01:25:10 +0000

    updated from database (includes strobe controller)

--- src/main/java/com/example/dmx/fixture/MiniWashFixtureController.java
+++ src/main/java/com/example/dmx/fixture/MiniWashFixtureController.java
@@ -16,42 +16,55 @@ import com.randomnoun.dmx.fixture.FixtureController;
  */
 public class MiniWashFixtureController extends FixtureController {
 
-	public MiniWashFixtureController(Fixture fixture) {
-		super(fixture);
-	}
-	
-	/** Sets the color of this fixture.
-	 * 
-	 * This sets the red, green and blue dimmers to the values for this color,
-	 * and sets the dimmer/strobe macro channel to open. 
-	 * 
-	 * @param color The color to set the light to.
-	 */
-	public void setColor(Color color) {
-		fixture.setDmxChannelValue(6, color.getRed());
-		fixture.setDmxChannelValue(7, color.getGreen());
-		fixture.setDmxChannelValue(8, color.getBlue());
-		fixture.setDmxChannelValue(5, 255);
-	}
-	
-	public void setMasterDimmer(int value) {
-		//super.setMasterDimmer(value);
-		fixture.setDmxChannelValue(5, 8 + ((255-value)*126)/255);
-	}
-	
+  public MiniWashFixtureController(Fixture fixture) {
+    super(fixture);
+  }
+  
+  /** Sets the color of this fixture.
+   * 
+   * This sets the red, green and blue dimmers to the values for this color,
+   * and sets the dimmer/strobe macro channel to open. 
+   * 
+   * @param color The color to set the light to.
+   */
+  public void setColor(Color color) {
+    fixture.setDmxChannelValue(6, color.getRed());
+    fixture.setDmxChannelValue(7, color.getGreen());
+    fixture.setDmxChannelValue(8, color.getBlue());
+    int dimmerStrobeValue = fixture.getDmxChannelValue(5);
+    if (dimmerStrobeValue >= 135 && dimmerStrobeValue <= 239) {
+      // currently strobing, don't set intensity (this will disable strobe)
+    } else {
+      fixture.setDmxChannelValue(5, 255);
+    }
+  }
+  
+  public void setMasterDimmer(int value) {
+    //super.setMasterDimmer(value);
+    fixture.setDmxChannelValue(5, 8 + ((255-value)*126)/255);
+  }
 
-	public void setMovementSpeed(int i) {
-		fixture.setDmxChannelValue(4, i);
-	}
+  public void setStrobe(int value) {
+    fixture.setDmxChannelValue(5, 135 + (value*104/255));
+  }
+  
+  public void unsetStrobe() {
+    fixture.setDmxChannelValue(5, 0);
+  }
 
-	public void setColorMacro(int i) {
-		MacroChannelDef mcd = (MacroChannelDef) fixture.getFixtureDef().getChannelDefByOffset(9);
-		fixture.setDmxChannelValue(9, mcd.getMacros().get(9).getLowValue());
-	}
 
-	public void setMovementMacro(int i) {
-		MacroChannelDef mcd = (MacroChannelDef) fixture.getFixtureDef().getChannelDefByOffset(11);
-		fixture.setDmxChannelValue(11, mcd.getMacros().get(11).getLowValue());
-	}
-	
+  public void setMovementSpeed(int i) {
+    fixture.setDmxChannelValue(4, i);
+  }
+
+  public void setColorMacro(int i) {
+    MacroChannelDef mcd = (MacroChannelDef) fixture.getFixtureDef().getChannelDefByOffset(9);
+    fixture.setDmxChannelValue(9, mcd.getMacros().get(9).getLowValue());
+  }
+
+  public void setMovementMacro(int i) {
+    MacroChannelDef mcd = (MacroChannelDef) fixture.getFixtureDef().getChannelDefByOffset(11);
+    fixture.setDmxChannelValue(11, mcd.getMacros().get(11).getLowValue());
+  }
+  
 }
