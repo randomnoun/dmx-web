2cea695f3f02a0c1f33b15d6e7418ac4c567c30c
Greg Knox <knoxg@randomnoun.com>
2012-05-08 20:24:44 +0000

    comment only

--- src/main/java/com/randomnoun/dmx/channel/BitResolution.java
+++ src/main/java/com/randomnoun/dmx/channel/BitResolution.java
@@ -1,3 +1,24 @@
 package com.randomnoun.dmx.channel;
 
-public enum BitResolution { BYTE, WORDHIGH, WORDLOW }
+/** 8 and 16-bit value encodings, used when creating channel definitions spanning 
+ * more than one DMX offset. 
+ * 
+ * <p>Each DMX offset is assigned a BitResolution value, which
+ * for single-byte values will be {@link BitResolution#BYTE}, and for two-byte values
+ * will be one of either {@link BitResolution#WORDHIGH} or {@link BitResolution#WORDLOW}
+ * 
+ * @author knoxg
+ */
+public enum BitResolution { 
+
+	/** Constant to define a one-byte (8-bit) value */
+	BYTE, 
+	
+	/** Constant to define the high (larger) byte of a two-byte (16-bit) value.
+	 */
+	WORDHIGH, 
+
+	/** Constant to define the low (smaller) byte of a two-byte (16-bit) value.
+	 */
+	WORDLOW 
+}
