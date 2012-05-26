95f2f1b88a95dc038ae4d52043ac0a8a99bbcafe
Greg Knox <knoxg@randomnoun.com>
2012-05-26 07:15:51 +0000

    pan/tilts are expressed as percentages to handle multiple fixture ranges. perhaps. probably want to check this later.

--- src/main/java/com/randomnoun/dmx/show/editor/FixturePanTiltCommand.java
+++ src/main/java/com/randomnoun/dmx/show/editor/FixturePanTiltCommand.java
@@ -3,6 +3,7 @@ package com.randomnoun.dmx.show.editor;
 import java.util.List;
 
 import com.randomnoun.dmx.fixture.Fixture;
+import com.randomnoun.dmx.fixture.FixtureDef;
 
 public class FixturePanTiltCommand extends FixtureCommand {
 	double panPosition, tiltPosition;
@@ -14,8 +15,9 @@ public class FixturePanTiltCommand extends FixtureCommand {
 	public void run() {
 		for (Fixture f : fixtures) { 
 			// @TODO as %age of pan/tilt range; see below
-			f.getFixtureController().panTo(panPosition); 
-			f.getFixtureController().tiltTo(tiltPosition);
+			FixtureDef fd = f.getFixtureDef();
+			f.getFixtureController().panTo(fd.getPanRange() * panPosition / 100); 
+			f.getFixtureController().tiltTo(fd.getTiltRange() * tiltPosition / 100);
 		}
 	}
 	public String toJava() {
