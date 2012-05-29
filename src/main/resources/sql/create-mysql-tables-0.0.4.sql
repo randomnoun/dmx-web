8ae401eee9fd93058814def16e0ec674c6e8d97e
Greg Knox <knoxg@randomnoun.com>
2012-05-29 03:16:29 +0000

    include column denoting whether show is recorded

---
+++ src/main/resources/sql/create-mysql-tables-0.0.4.sql
@@ -0,0 +1,7 @@
+ALTER TABLE `showdef` 
+  ADD COLUMN `ynRecorded` VARCHAR(1) NOT NULL DEFAULT "N" COMMENT 'set to "Y" if this show extends RecordedShow' AFTER `javadoc`;
+  
+UPDATE showDef SET ynRecorded='N';
+
+
+
