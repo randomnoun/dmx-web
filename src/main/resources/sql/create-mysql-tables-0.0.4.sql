014b2d9e745a7896e7d1a87ddb894873620d4b8b
Greg Knox <knoxg@randomnoun.com>
2012-05-29 21:47:17 +0000

    case sensistive on unix

--- src/main/resources/sql/create-mysql-tables-0.0.4.sql
+++ src/main/resources/sql/create-mysql-tables-0.0.4.sql
@@ -1,7 +1,4 @@
-ALTER TABLE `showdef` 
+ALTER TABLE `showDef`
   ADD COLUMN `ynRecorded` VARCHAR(1) NOT NULL DEFAULT "N" COMMENT 'set to "Y" if this show extends RecordedShow' AFTER `javadoc`;
-  
+
 UPDATE showDef SET ynRecorded='N';
-
-
-
