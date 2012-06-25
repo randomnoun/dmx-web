813691d41136915c60c1d90d8d43436370d872f1
Greg Knox <knoxg@randomnoun.com>
2012-06-25 09:08:36 +0000

    BeanshellTest (wip)

---
+++ src/test/java/com/randomnoun/dmx/BeanshellTest.java
@@ -0,0 +1,22 @@
+package com.randomnoun.dmx;
+
+import junit.framework.TestCase;
+
+public class BeanshellTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+	public void testBeanshellScripts() {
+		// instantiate a beanshell engine,
+		// load up the names of all fixtureDef/controller/muxer classes
+		// and showDefs
+		// and then try to instantiate them.
+	}
+
+}
