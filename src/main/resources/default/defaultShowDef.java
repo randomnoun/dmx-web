bc794f5bf4b110a6d5e94afb29440e8d295d1a6a
Greg Knox <knoxg@randomnoun.com>
2012-09-07 23:21:53 +0000

    default and recorded shows now use Show.init() method

--- src/main/resources/default/defaultShowDef.java
+++ src/main/resources/default/defaultShowDef.java
@@ -8,98 +8,86 @@ import com.randomnoun.dmx.Controller;
 import com.randomnoun.dmx.audioController.AudioController;
 import com.randomnoun.dmx.show.Show;
 
-/** Flash the lights and play 'smooth criminal'.
- *
- * If nothing happens in ten seconds, the volume will fade down to 50% over the next 10 seconds.
- * If it still isn't cancelled, this show will loop the music after three minutes.
+/** This documentation will be displayed as a tooltip in the Show panel when
+ * the user hovers their mouse over the button representing this show.
+ * 
  */
 public class DefaultShow extends Show {
 
-  Logger logger = Logger.getLogger(DefaultShow.class);
+    Logger logger = Logger.getLogger(DefaultShow.class);
   
-  FixtureController leftWash;
-  FixtureController rightWash;
-  AudioController audioController;
+    // FixtureController demoLight;
   
-  public DefaultShow(long id, Controller controller, Properties properties) {
-    super(id, controller, "Performer entrance", Long.MAX_VALUE, properties);
-    sleepMonitor = new Object();
-    leftWash = controller.getFixtureControllerByName("leftWash");
-    rightWash = controller.getFixtureControllerByName("rightWash");
-    audioController = controller.getAudioController();
-  }
-  
-  public void pause() {}
-  public void stop() {}
-  
-  protected void reset() {
-    super.reset();
-    logger.debug("reset()");
-    leftWash.setColor(Color.BLACK);
-    rightWash.setColor(Color.BLACK);
-    leftWash.setMovementSpeed(0);
-    rightWash.setMovementSpeed(0);
-    leftWash.panTo(90); leftWash.tiltTo(45);
-    rightWash.panTo(270); rightWash.tiltTo(45);
+    public DefaultShow(long id, Controller controller, Properties properties) {
+        super(id, controller, "Performer entrance", Long.MAX_VALUE, properties);
+        audioController = controller.getAudioController();
     
-    // @TODO wait until muxers say that the fixtures
-    // are in the right position
-  }
-  
-  public void play() {
-    logger.debug("play()");
-    reset();
-    waitUntil(1000);
-    if (isCancelled()) { return; }
-    
-    logger.debug("play() part 2");
-    long songStartTime = getShowTime();
-    audioController.playAudioFile("smoothCriminal.mp3");
-    
-    Color[] chaseColors = new Color[]{
-      Color.GREEN,
-      Color.BLUE,
-      Color.RED
-    };
-    // 20 steps at half a second per chase step
-    int i=0;
-    while (getShowTime() < 10 * 1000) {
-      i++;
-      leftWash.setColor(chaseColors[i % 3]);
-      rightWash.setColor(chaseColors[(i+1) % 3]);
-      waitFor(500);
-      if (isCancelled()) { return; }
+        // demoLight = controller.getFixtureControllerByName("shiny light");
     }
-    
-    // fade music down
-    double volume = 100;
-    long startFadeTime = getShowTime();
-    long stopFadeTime = getShowTime() + 20 * 1000;
-    while (getShowTime() < stopFadeTime) {
-      i++;
-      // volume = volume - (50 / 20);
-      volume = 100 - (getShowTime() - startFadeTime) * 50 / (stopFadeTime-startFadeTime);
-      leftWash.setColor(chaseColors[i % 3]);
-      rightWash.setColor(chaseColors[(i+1) % 3]);
-      audioController.setVolume(volume);
-      waitFor(500);
-      if (isCancelled()) { return; }
-    }
-    
-    while (true) {
-      while (getShowTime() - songStartTime < 180 * 1000) {
-        i++;
-        leftWash.setColor(chaseColors[i % 3]);
-        rightWash.setColor(chaseColors[(i+1) % 3]);
-        waitFor(500);
-        if (isCancelled()) { return; }
-      }
-      songStartTime = getShowTime();
-      audioController.playAudioFile("smoothCriminal.mp3");
-      audioController.setVolume(50.0);
-    }
-    
-  }
   
+    public void pause() {}
+    public void stop() {}
+  
+    protected void reset() {
+		super.reset();
+		logger.debug("reset()");
+		
+		// demoLight.setColor(Color.BLACK);
+		// demoLight.setMovementSpeed(0);
+		// demoLightWash.panTo(90); demoLight.tiltTo(45);
+    }
+  
+    public void play() {
+    
+    	logger.debug("play()");
+    	reset();
+    	
+    	/* demo show
+    	waitUntil(1000);
+    	if (isCancelled()) { return; }
+		logger.debug("play() part 2");
+		long songStartTime = getShowTime();
+		audioController.playAudioFile("smoothCriminal.mp3");
 
+		Color[] chaseColors = new Color[]{
+		  Color.GREEN,
+		  Color.BLUE,
+		  Color.RED
+		};
+		// 20 steps at half a second per chase step
+		int i=0;
+		while (getShowTime() < 10 * 1000) {
+			i++;
+			demoLight.setColor(chaseColors[i % 3]);
+			waitFor(500);
+			if (isCancelled()) { return; }
+	    }
+
+	    // fade music down
+	    double volume = 100;
+	    long startFadeTime = getShowTime();
+	    long stopFadeTime = getShowTime() + 20 * 1000;
+	    while (getShowTime() < stopFadeTime) {
+			i++;
+			// volume = volume - (50 / 20);
+			volume = 100 - (getShowTime() - startFadeTime) * 50 / (stopFadeTime-startFadeTime);
+			demoLight.setColor(chaseColors[i % 3]);
+			audioController.setVolume(volume);
+			waitFor(500);
+			if (isCancelled()) { return; }
+	    }
+
+	    while (true) {
+	    	while (getShowTime() - songStartTime < 180 * 1000) {
+				i++;
+				demoLight.setColor(chaseColors[i % 3]);
+				waitFor(500);
+				if (isCancelled()) { return; }
+		    }
+			songStartTime = getShowTime();
+			audioController.playAudioFile("smoothCriminal.mp3");
+			audioController.setVolume(50.0);
+		}
+		*/
+    }
 }  
--- src/main/resources/default/recordedShowDef.java
+++ src/main/resources/default/recordedShowDef.java
@@ -19,57 +19,57 @@ import com.randomnoun.dmx.show.editor.*;
  */
 public class {CLASSNAME_GOES_HERE} extends RecordedShow {
 
-  Logger logger = Logger.getLogger({CLASSNAME_GOES_HERE}.class);
+    Logger logger = Logger.getLogger({CLASSNAME_GOES_HERE}.class);
   
-  Recording recording;
-  AudioController audioController;
+    Recording recording;
+    AudioController audioController;
   
-  public {CLASSNAME_GOES_HERE}(long id, Controller controller, Properties properties) {
-    super(id, controller, "{SHOWNAME_GOES_HERE}", Long.MAX_VALUE, properties);
-    audioController = controller.getAudioController();
-    recording = getRecording(controller);
-  }
-  
-  // MODIFY WITH CAUTION
-  // 
-  // IT APPEARS THAT YOU'RE VIEWING THE SOURCE FOR A RECORDED SHOW.
-  // 
-  // You can edit this file, but any lines between the 
-  // "RECORDING DEFINITION START" and "RECORDING DEFINITION END" markers
-  // below will be replaced if the show is edited using the 'Get show recording'
-  // button from the show definitions maintenance page.
-  //
-  // You can prevent this from occurring by changing the 'RecordedShow' 
-  // superclass to 'Show'
-  //
-  public Recording getRecording(Controller controller) {
-	// *** RECORDING DEFINITION START -- DO NOT REMOVE THIS LINE ***
-{CODE_GOES_HERE}
-    // *** RECORDING DEFINITION END -- DO NOT REMOVE THIS LINE ***
-  }
-  
-  public void pause() {}
-  public void stop() {}
-  protected void reset() {
-    super.reset();
-    logger.debug("reset()");
-  }
-  
-  public void play() {
-    logger.debug("play()");
-    reset();
-    while (!isCancelled()) {
-      int frameCount = 0;
-      for (Frame f : recording.getFrames()) {
-    	frameCount++;
-    	setLabel("Frame " + frameCount + "/" + recording.getFrames().size());
-        for (Command c : f.getCommands()) {
-          c.run();
-        }
-        if (isCancelled()) { break; }
-        waitFor({DEFAULT_DELAY_GOES_HERE}); // @TODO allow speed to be set somewhere
-      }
+    public void init() {
+	    setLength(Long.MAX_VALUE);
+	    audioController = controller.getAudioController();
+	    recording = getRecording(controller);
     }
-  }
   
+	// MODIFY WITH CAUTION
+	// 
+	// IT APPEARS THAT YOU'RE VIEWING THE SOURCE FOR A RECORDED SHOW.
+	// 
+	// You can edit this file, but any lines between the 
+	// "RECORDING DEFINITION START" and "RECORDING DEFINITION END" markers
+	// below will be replaced if the show is edited using the 'Get show recording'
+	// button from the show definitions maintenance page.
+	//
+	// You can prevent this from occurring by changing the 'RecordedShow' 
+	// superclass to 'Show' at the beginning of this class definition.
+	//
+    public Recording getRecording(Controller controller) {
+    	// *** RECORDING DEFINITION START -- DO NOT REMOVE THIS LINE ***
+{CODE_GOES_HERE}
+		// *** RECORDING DEFINITION END -- DO NOT REMOVE THIS LINE ***
+    }
+  
+    public void pause() {}
+    public void stop() {}
+    protected void reset() {
+        super.reset();
+        logger.debug("reset()");
+    }
+  
+    public void play() {
+	    logger.debug("play()");
+	    reset();
+	    while (!isCancelled()) {
+			int frameCount = 0;
+			for (Frame f : recording.getFrames()) {
+		    	frameCount++;
+		    	setLabel("Frame " + frameCount + "/" + recording.getFrames().size());
+		        for (Command c : f.getCommands()) {
+		            c.run();
+		        }
+		        if (isCancelled()) { break; }
+		        waitFor({DEFAULT_DELAY_GOES_HERE}); // @TODO allow speed to be set somewhere
+			}
+	    }
+    }
+    
 }  
