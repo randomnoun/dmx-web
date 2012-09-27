7d7c080f73c24af4927553c609a93c527bf1fcec
Greg Knox <knoxg@randomnoun.com>
2012-09-27 00:23:23 +0000

    stageAttachment; comment fix

--- src/main/resources/sql/create-mysql-tables-0.0.8.sql
+++ src/main/resources/sql/create-mysql-tables-0.0.8.sql
@@ -1,10 +1,8 @@
--- guard SQL: return success if fisrt SQL statement executed successfully, fail if script not applied
+-- guard SQL: return success if first SQL statement executed successfully, fail if script not yet applied
 SELECT COUNT(*) FROM device;
 
 -- guard SQL: return success if last SQL statement executed successfully, fail if script not completely applied
-SELECT COUNT(*) FROM fixtureDefAttachment;
-
-
+SELECT COUNT(*) FROM stageAttachment;
 
 CREATE TABLE `device` (
   `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -12,7 +10,7 @@ CREATE TABLE `device` (
   `className` VARCHAR(255) NOT NULL COMMENT 'java class type',
   `type` VARCHAR(1) NOT NULL COMMENT 'D=DMX, S=audioSource, C=audioController',
   `ynActive` VARCHAR(1) NOT NULL,
-  `universeNumber` INTEGER UNSIGNED COMMENT 'type=D only',
+  `universeNumber` INTEGER UNSIGNED COMMENT '1-based universe number',
   PRIMARY KEY (`id`)
 )
 ENGINE = InnoDB;
@@ -67,3 +65,41 @@ FROM fixtureDefImage;
 
 DROP TABLE fixtureDefImage;
 
+ALTER TABLE `fixtureDefAttachment` 
+ ADD COLUMN `width` INTEGER UNSIGNED DEFAULT NULL COMMENT 'image/video' AFTER `description`,
+ ADD COLUMN `height` INTEGER UNSIGNED DEFAULT NULL COMMENT 'image/video' AFTER `width`,
+ ADD COLUMN `length` INTEGER UNSIGNED DEFAULT NULL COMMENT 'audio/video (msec)/anim (frames)' AFTER `height`,
+ ADD COLUMN `audioRate` INTEGER UNSIGNED DEFAULT NULL COMMENT 'audio samples/sec' AFTER `length`,
+ ADD COLUMN `videoRate` INTEGER UNSIGNED DEFAULT NULL COMMENT 'video fps * 1000' AFTER `audioRate`,
+ ADD COLUMN `audioCodec` VARCHAR(100) DEFAULT NULL COMMENT 'encapsulated audio format' AFTER `videoRate`,
+ ADD COLUMN `videoCodec` VARCHAR(100) DEFAULT NULL COMMENT 'encapsulated video format' AFTER `audioCodec`;
+
+ALTER TABLE `showDefAttachment` 
+ ADD COLUMN `width` INTEGER UNSIGNED DEFAULT NULL COMMENT 'image/video' AFTER `description`,
+ ADD COLUMN `height` INTEGER UNSIGNED DEFAULT NULL COMMENT 'image/video' AFTER `width`,
+ ADD COLUMN `length` INTEGER UNSIGNED DEFAULT NULL COMMENT 'audio/video (msec)/anim (frames)' AFTER `height`,
+ ADD COLUMN `audioRate` INTEGER UNSIGNED DEFAULT NULL COMMENT 'audio samples/sec' AFTER `length`,
+ ADD COLUMN `videoRate` INTEGER UNSIGNED DEFAULT NULL COMMENT 'video fps * 1000' AFTER `audioRate`,
+ ADD COLUMN `audioCodec` VARCHAR(100) DEFAULT NULL COMMENT 'encapsulated audio format' AFTER `videoRate`,
+ ADD COLUMN `videoCodec` VARCHAR(100) DEFAULT NULL COMMENT 'encapsulated video format' AFTER `audioCodec`;
+
+CREATE TABLE `stageAttachment` (
+  `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  `stageId` INTEGER UNSIGNED NOT NULL,
+  `name` VARCHAR(200) NOT NULL,
+  `size` INTEGER UNSIGNED NOT NULL,
+  `contentType` VARCHAR(200) NOT NULL,
+  `fileLocation` VARCHAR(200) NOT NULL COMMENT 'relative to app base specified in appConfig',
+  `description` VARCHAR(255) NOT NULL,
+  `width` INTEGER UNSIGNED DEFAULT NULL COMMENT 'image/video',
+  `height` INTEGER UNSIGNED DEFAULT NULL COMMENT 'image/video',
+  `length` INTEGER UNSIGNED DEFAULT NULL COMMENT 'audio/video (msec)/anim (frames)',
+  `audioRate` INTEGER UNSIGNED DEFAULT NULL COMMENT 'audio samples/sec',
+  `videoRate` INTEGER UNSIGNED DEFAULT NULL COMMENT 'video fps * 1000',
+  `audioCodec` VARCHAR(100) DEFAULT NULL COMMENT 'encapsulated audio format',
+  `videoCodec` VARCHAR(100) DEFAULT NULL COMMENT 'encapsulated video format',
+  PRIMARY KEY (`id`)
+)
+ENGINE = InnoDB;
+
+ 
